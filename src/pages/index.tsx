cbbd459075799e6da2345feb9ad4857ebaaf5a24
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-03-24 17:45:59 +0100

    new youtube video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,60 @@
-import NavBar from '@/components/NavBar';
-import SocialBox from '@/components/SocialBox';
-import LinkBox from '@/components/LinkBox';
-import SmallLinkBox from '@/components/SmallLinkBox';
-import Footer from '@/components/Footer';
+import NavBar from "@/components/NavBar";
+import SocialBox from "@/components/SocialBox";
+import LinkBox from "@/components/LinkBox";
+import SmallLinkBox from "@/components/SmallLinkBox";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-      <div className='md:flex hidden flex-col m-auto gap-10 max-w-[700px] text-white h-full'>
-        <main className='md:flex flex-col m-auto gap-10 max-w-[700px] text-white'>
+      <div className="md:flex hidden flex-col m-auto gap-10 max-w-[700px] text-white h-full">
+        <main className="md:flex flex-col m-auto gap-10 max-w-[700px] text-white">
           <NavBar />
           <section>
-            <p className='satoshi-regular text-lg mb-2'>Social Links</p>
+            <p className="satoshi-regular text-lg mb-2">Social Links</p>
             <SocialBox />
           </section>
           <section>
-            <p className='satoshi-regular text-lg mb-2'>
+            <p className="satoshi-regular text-lg mb-2">
               Participa en mi Hackathon
             </p>
             <LinkBox
-              title='La Hackathon Del Dev'
-              buttonText='Ir a la web'
-              link='https://www.lahackathondeldev.com/'
+              title="La Hackathon Del Dev"
+              buttonText="Ir a la web"
+              link="https://www.lahackathondeldev.com/"
             />
           </section>
           <section>
-            <p className='satoshi-regular text-lg mb-2'>CodeCrafters</p>
+            <p className="satoshi-regular text-lg mb-2">
+              Último vídeo de YouTube
+            </p>
             <LinkBox
-              title='Proyectos que van mucho más allá de lo básico'
-              buttonText='Más información'
-              link='https://app.codecrafters.io/join?via=jmontes33'
+              title="Mi Mayor Proyecto Personal"
+              buttonText="Ver vídeo"
+              link="https://infiniteurl.com/la-hackathon-del-dev"
             />
           </section>
-          <section className='flex flex-col'>
-            <p className='satoshi-regular text-lg mb-2'>Otros links</p>
-            <div className='flex flex-col gap-5'>
-              <div className='flex flex-col gap-5'>
+          <section>
+            <p className="satoshi-regular text-lg mb-2">CodeCrafters</p>
+            <LinkBox
+              title="Proyectos que van mucho más allá de lo básico"
+              buttonText="Más información"
+              link="https://app.codecrafters.io/join?via=jmontes33"
+            />
+          </section>
+          <section className="flex flex-col">
+            <p className="satoshi-regular text-lg mb-2">Otros links</p>
+            <div className="flex flex-col gap-5">
+              <div className="flex flex-col gap-5">
                 <SmallLinkBox
-                  title='Máster en Programación para Creativos de LABASAD'
-                  link='https://hubs.ly/Q02kHdR40'
+                  title="Máster en Programación para Creativos de LABASAD"
+                  link="https://hubs.ly/Q02kHdR40"
                 />
               </div>
-              <div className='flex flex-col gap-5'>
+              <div className="flex flex-col gap-5">
                 <SmallLinkBox
-                  title='Roadmaps detallados para desarrolladores'
-                  link='https://roadmap.sh/'
+                  title="Roadmaps detallados para desarrolladores"
+                  link="https://roadmap.sh/"
                 />
               </div>
             </div>
@@ -52,51 +62,61 @@ export default function Home() {
         </main>
         <Footer />
       </div>
-      <div className='flex md:hidden flex-col px-5 text-white'>
-        <main className='flex md:hidden flex-col gap-10'>
+      <div className="flex md:hidden flex-col px-5 text-white">
+        <main className="flex md:hidden flex-col gap-10">
           <NavBar />
-          <section className='flex flex-col gap-5'>
+          <section className="flex flex-col gap-5">
             <section>
-              <p className='satoshi-regular text-lg mb-2'>Social Links</p>
+              <p className="satoshi-regular text-lg mb-2">Social Links</p>
               <SocialBox />
             </section>
             <section>
-              <p className='satoshi-regular text-lg mb-2'>
+              <p className="satoshi-regular text-lg mb-2">
                 Participa en mi Hackathon
               </p>
               <LinkBox
-                title='La Hackathon Del Dev'
-                buttonText='Ir a la web'
-                link='https://www.lahackathondeldev.com/'
+                title="La Hackathon Del Dev"
+                buttonText="Ir a la web"
+                link="https://www.lahackathondeldev.com/"
               />
             </section>
             <section>
-              <p className='satoshi-regular text-lg mb-2'>CodeCrafters</p>
+              <p className="satoshi-regular text-lg mb-2">
+                Último vídeo de YouTube
+              </p>
               <LinkBox
-                title='Proyectos que van mucho más allá de lo básico'
-                buttonText='Más información'
-                link='https://app.codecrafters.io/join?via=jmontes33'
+                title="Mi Mayor Proyecto Personal"
+                buttonText="Ver vídeo"
+                link="https://infiniteurl.com/la-hackathon-del-dev"
+              />
+            </section>
+            <section>
+              <p className="satoshi-regular text-lg mb-2">CodeCrafters</p>
+              <LinkBox
+                title="Proyectos que van mucho más allá de lo básico"
+                buttonText="Más información"
+                link="https://app.codecrafters.io/join?via=jmontes33"
               />
             </section>
           </section>
-          <section className='flex flex-col mb-10'>
-            <p className='satoshi-regular text-lg mb-2'>Otros links</p>
-            <div className='flex flex-col gap-5'>
-              <div className='flex flex-col gap-5'>
+          <section className="flex flex-col mb-10">
+            <p className="satoshi-regular text-lg mb-2">Otros links</p>
+            <div className="flex flex-col gap-5">
+              <div className="flex flex-col gap-5">
                 <SmallLinkBox
-                  title='Máster Online en Programación para Creativos de LABASAD'
-                  link='https://hubs.ly/Q02kHdR40'
+                  title="Máster Online en Programación para Creativos de LABASAD"
+                  link="https://hubs.ly/Q02kHdR40"
                 />
               </div>
-              <div className='flex flex-col gap-5'>
+              <div className="flex flex-col gap-5">
                 <SmallLinkBox
-                  title='Roadmaps detallados para desarrolladores'
-                  link='https://roadmap.sh/'
+                  title="Roadmaps detallados para desarrolladores"
+                  link="https://roadmap.sh/"
                 />
               </div>
             </div>
           </section>
-          <section className='flex justify-center'></section>
+          <section className="flex justify-center"></section>
         </main>
         <Footer />
       </div>
